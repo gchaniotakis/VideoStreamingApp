272347d7a8e817a76379ff43245831b7a059c4e8
George Chaniotakis <haniotinho@gmail.com>
2019-02-06 19:07:04 +0200

    created migrations

---
+++ VideoStreamingApp/Migrations/201902061706135_First Migration.cs
@@ -0,0 +1,30 @@
+namespace VideoStreamingApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FirstMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Movies",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Genre = c.Int(nullable: false),
+                        Duration = c.Int(nullable: false),
+                        Trailer_Url = c.String(),
+                        Thumbnail = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Movies");
+        }
+    }
+}
--- VideoStreamingApp/Models/MovieDb.cs
+++ VideoStreamingApp/Models/MovieDb.cs
@@ -6,28 +6,18 @@ namespace VideoStreamingApp.Models
 
     public class MovieDb : DbContext
     {
-        // Your context has been configured to use a 'MovieDb' connection string from your application's 
-        // configuration file (App.config or Web.config). By default, this connection string targets the 
-        // 'VideoStreamingApp.Models.MovieDb' database on your LocalDb instance. 
-        // 
-        // If you wish to target a different database and/or database provider, modify the 'MovieDb' 
-        // connection string in the application configuration file.
+
         public MovieDb()
             : base("name=MovieDb")
         {
         }
 
-        // Add a DbSet for each entity type that you want to include in your model. For more information 
-        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
 
         public virtual DbSet<Movie> Movies { get; set; }
 
-        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+       
     }
 
-    //public class MyEntity
-    //{
-    //    public int Id { get; set; }
-    //    public string Name { get; set; }
-    //}
+
 }
