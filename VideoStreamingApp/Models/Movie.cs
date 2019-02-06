d1bd566dbf06f0e420aed70fd56ff93fb21cc7e9
George Chaniotakis <haniotinho@gmail.com>
2019-02-06 18:55:24 +0200

    movie model created

---
+++ VideoStreamingApp/Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VideoStreamingApp.Models
+{
+    public class Movie
+    {
+        public int id { get; set; }
+        public string Title { get; set; }
+        public Genre Genre { get; set; } 
+        public int Duration { get; set; }
+        public string Trailer_Url { get; set; }
+        public string Thumbnail { get; set;  }
+    }
+
+    public enum Genre
+    {
+        Action, 
+        Horror,
+        ScienceFiction,
+        Comedy
+    }
+}
